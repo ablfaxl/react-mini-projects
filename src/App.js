f1934bfd5e12c4688e616ba66b96da8387893ef8
ablfaxl <taghavi.abolfazl@outlook.com>
2023-11-10 23:08:39 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePervious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 && "active"}`}> 1</div>
+            <div className={`${step >= 2 && "active"}`}> 2</div>
+            <div className={`${step >= 3 && "active"}`}> 3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              disabled={step === 1}
+              onClick={handlePervious}
+              style={{
+                backgroundColor: step === 1 ? "#cccccc" : "#7950f2",
+                color: step === 1 ? "" : "#fff",
+              }}
+            >
+              Pervious
+            </button>
+            <button
+              disabled={step === 3}
+              onClick={handleNext}
+              style={{
+                backgroundColor: step === 3 ? "#cccccc" : "#7950f2",
+                color: step === 1 ? "" : "#fff",
+              }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
