f5577b8ccd547a85d1d72e34488dc64bb32abbbd
abolfazl taghavi <taghavi.abolfazl@outlook.com>
2023-11-11 15:50:43 +0330

    add first config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-import React from "react";
-
 export default function App() {
-  return <div>Hello FarAway Project</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <From />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>Far Away</h1>;
+}
+
+function From() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip ?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <em>
+      <footer className="stats">
+        You have X items on your list, and you already packed X%
+      </footer>
+    </em>
+  );
 }
