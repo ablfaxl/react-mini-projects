882b8ddb892e58202d0d7b5c7158f7b14f280377
abolfazl taghavi <taghavi.abolfazl@outlook.com>
2023-11-18 12:07:09 +0330

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-//075
+//076
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
