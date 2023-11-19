9d143ec5ec6543f1ddc4600381ffa5dd786bb1fa
abolfazl taghavi <taghavi.abolfazl@outlook.com>
2023-11-19 16:56:09 +0330

    add sort methods

--- src/App.js
+++ src/App.js
@@ -87,10 +87,24 @@ function From({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleIetms }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(b.packed) - Number(a.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -99,6 +113,13 @@ function PackingList({ items, onDeleteItem, onToggleIetms }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed</option>
+        </select>
+      </div>
     </div>
   );
 }
