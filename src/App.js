728074a4b0c26a2e63ce91e828740696c67662cc
abolfazl taghavi <taghavi.abolfazl@outlook.com>
2023-11-19 17:54:06 +0330

    89

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Form from "./components/Form";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
 
-//088
+//089
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
