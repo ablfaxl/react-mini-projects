e1ff2bbd145f4b8bf4a2c882dbd582386f411458
ablfaxl <taghavi.abolfazl@outlook.com>
2023-11-25 22:03:11 +0330

    create compostion component to fix prop drilling

--- src/App.js
+++ src/App.js
@@ -54,18 +54,28 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Numresults movies={movies} />
+      </NavBar>
+      <Main movies={movies}>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
-    <nav className="nav-bar">
-      <Logo />
+    <nav
+      className="
+    nav-bar"
+    >
       <Search />
-      <Numresults movies={movies} />
+      <Logo />
+      {children}
     </nav>
   );
 }
@@ -99,16 +109,11 @@ function Numresults({ movies }) {
   );
 }
 
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -118,7 +123,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
