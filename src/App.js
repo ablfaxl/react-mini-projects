c1194180bac0ba861809972faa2daa018f2367f9
ablfaxl <taghavi.abolfazl@outlook.com>
2023-11-25 22:27:13 +0330

    change children to element

--- src/App.js
+++ src/App.js
@@ -59,13 +59,15 @@ export default function App() {
         <Numresults movies={movies} />
       </NavBar>
       <Main movies={movies}>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
-        <Box>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
-        </Box>
+        <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList watched={watched} />
+            </>
+          }
+        />
       </Main>
     </>
   );
@@ -73,10 +75,7 @@ export default function App() {
 
 function NavBar({ children }) {
   return (
-    <nav
-      className="
-    nav-bar"
-    >
+    <nav className="nav-bar">
       <Logo />
       <Search />
       {children}
@@ -117,14 +116,14 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({ children }) {
+function Box({ element }) {
   const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="box">
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && children}
+      {isOpen && element}
     </div>
   );
 }
